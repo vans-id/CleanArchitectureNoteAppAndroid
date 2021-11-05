76b8e1b841b28026694170424ba9bf2901929c27
Evan <djevann11@gmail.com>
2021-11-05 11:25:30 +0700

    setup project structure

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.plcoding.cleanarchitecturenoteapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF252525)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0xffffab91)
+val RedPink = Color(0xfff48fb1)
+val BabyBlue = Color(0xff81deea)
+val Violet = Color(0xffcf94da)
+val LightGreen = Color(0xffe7ed9b)
+
+val creamOrange = Color(0xFFFFAB91)
+val deepYellow = Color(0xFFFFCC80)
+val lime = Color(0xFFE7ED9B)
+val softBlue = Color(0xFF81DEEA)
+val deepGreen = Color(0xFF7FCBC3)
+val softPink = Color(0xFFF48FB1)
+val lavender = Color(0xFFCF94DA)
